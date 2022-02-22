df57b77a2558d407fc7ca1fbccd04822cd6c7e08
Troll <trino@mail.ru>
2022-02-22 23:23:00 +0300

    add to cart_get_number_of_items function

--- public/app.js
+++ public/app.js
@@ -14,4 +14,22 @@ function add_to_card(id)
 	var x = window.localStorage.getItem(key);
 	x = x * 1 + 1;
 	window.localStorage.setItem(key, x);
+
+	alert('Items in your cart: ' + cart_get_number_of_items()); //выводит на экран окно с колличеством пицц в корзине
+}
+
+function cart_get_number_of_items() 
+{
+	var cnt = 0
+	for(var i = 0; i < window.localStorage.length; i++)
+	{
+		var key = window.localStorage.key(i);	// получаю ключ
+		var value = window.localStorage.getItem(key);	// получаю значение, аналог в руби hh[key] = x
+
+		if (key.indexOf('product_') == 0)
+		{
+			cnt = cnt + value * 1; // увеличиваю на 1
+		}
+	}
+	return cnt
 }
